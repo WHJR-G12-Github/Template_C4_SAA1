f7b2c0c9a6ae7210a227dff917dbf3f0a3207aaf
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-14 12:27:09 +0530

    Create template_c4_saa1.py

---
+++ template_c4_saa1.py
@@ -0,0 +1,7 @@
+# Define a function 'product()' with 'num1','num2' as parameters
+
+  # Calculate the 'result' by multiplying 'num1' and 'num2'
+  
+  # Return the 'result'
+  
+# Call the function 'product()' to multiply '29' and '34' and print the  returned 'result'
